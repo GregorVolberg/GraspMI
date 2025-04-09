fa0a9bf0cbb0a7aca3f8eb10eba11c0d5289de4e
Gregor Volberg <vog20246@pc1032250291.ads.uni-regensburg.de>
2025-04-09 10:06:58 +0200

    Rev 4

--- task/graspMI_practice.m
+++ task/graspMI_practice.m
@@ -1,26 +1,23 @@
 % =======================
 % function[] = graspMI_practice()
-% Two practice runs with 12 trials each and no saving of results
+% Two runs with 9 trials each
 % ========================
 function[] = graspMI_practice()
 
-clear all
+test_run = 0;
 
 %% set up paths, responses, monitor, ...
 addpath('./func'); 
 rawdir = ['./'];
 
-[vp, starting_condition, color_mapping, msf_colors, msf_text] = get_experimentInfo;
-MonitorSelection = 3; % 6 in EEG
+[vp, starting_condition, ~, msf_colors, msf_text] = get_experimentInfo;
+MonitorSelection = 3; % 6 in EEG, 3 in office, 4 in home office
 MonitorSpecs = getMonitorSpecs(MonitorSelection); % subfunction, gets specification for monitor
 
 %% PTB         
 AssertOpenGL;
-Screen('Preference', 'SkipSyncTests', 1); % Sync test will not work with Windows 10
+Screen('Preference', 'SkipSyncTests', 0); % may be set to 1 for testing
 Screen('Preference', 'TextRenderer', 0); 
-PsychImaging('PrepareConfiguration');
-PsychImaging('AddTask', 'General', 'FloatingPoint32BitIfPossible');
-PsychImaging('FinalizeConfiguration');    
 
 %% Prepare ViewPixx marker write-out
 topLeftPixel = [0 0 1 1];
@@ -29,16 +26,17 @@ setVpixxMarker  = @(windowPointer, value) Screen('FillRect', windowPointer, [val
 
 %% Response buttons
 KbName('UnifyKeyNames');
-TastenCodes  = KbName({'y', 'x', 'c', 'v', 'ESCAPE'}); % [89, 88, 67, 86, 27]
+%TastenCodes  = KbName({'y', 'x', 'c', 'v', 'ESCAPE'}); % [89, 88, 67, 86, 27]
+TastenCodes  = KbName({'m', ',<', '.>', '-_', 'ESCAPE'}); % 77   188   190   189    27
 TastenVector = zeros(1,256); TastenVector(TastenCodes) = 1;
 
 %% Stimulus
-FixCrossSize  = 1; % in degrees visual angle
+FixCrossSize  = 2; % in degrees visual angle
 FixCrossPixel = (FixCrossSize * MonitorSpecs.PixelsPerDegree) - ...
                  mod((FixCrossSize * MonitorSpecs.PixelsPerDegree), 2); % in pixels, make it an even number
 FixCross      = [-FixCrossPixel/2, FixCrossPixel/2, 0, 0;
                  0, 0, -FixCrossPixel/2, FixCrossPixel/2]; % coordinates for drawing
-FixCrossWidth = 4; % line width, in pixels             
+FixCrossWidth = 10; % line width, in pixels             
 
 %% Colors
 colorBlack = [0 0 0];
@@ -47,18 +45,21 @@ textSize = 30;
 textSizeQuestionMark = 60;
 
 %% Timing of presentation
-% ISI      = [2 3]; % inter-stimulus interval (s); 
-% CueTime  = 1.5;   % cue presentation time (s); 180 frames @ 120 Hz
-% TaskTime = 3;     % target (go signal for movement / imagery) presentation time (s); 360 frames @ 120 Hz 
-% timeOut  = 2;     % question mark or blank presentation time (s), also time-out for reponse; 240 frames @ 120 Hz
-% BreakBetweenBlocks = 120; % pause between blocks (s)
-
-% use for testing:
-ISI      = [0 0.1]; 
-CueTime  = 0.1;
-TaskTime = 0.1;
-timeOut  = 0.1;
-BreakBetweenBlocks = 1;
+if test_run == 0
+ ISI      = [2 3]; % inter-stimulus interval (s); 
+ CueTime  = 1.5;   % cue presentation time (s); 180 frames @ 120 Hz
+ TaskTime = 3;     % target (go signal for movement / imagery) presentation time (s); 360 frames @ 120 Hz 
+ timeOut  = 2;     % question mark or blank presentation time (s), also time-out for reponse; 240 frames @ 120 Hz
+ BreakBetweenBlocks = 30; % pause between blocks (s)
+ W = inf;          % wait time for kbQueueWait
+elseif test_run == 1
+ ISI      = [0.2 0.8]; 
+ CueTime  = 0.5;
+ TaskTime = 0.5;
+ timeOut  = 0.5;
+ BreakBetweenBlocks = 5;
+ W = 1;
+end
 
 %% results file
 GraspMI      = [];
@@ -67,13 +68,13 @@ outfilename   = ['sub-', vp, '_task-Grasping.mat'];
 
 %% messages
 msgEnd = 'Experiment Ended!\n\nPlease wait until the researcher turned off the recordings.';
-msgBreak = 'Take a break (2 min)';
+msgBreak = 'Take a break (30 s)';
 
 try
     Priority(1);
     HideCursor;
     [win, MonitorDimension] = Screen('OpenWindow', MonitorSpecs.ScreenNumber, 127); 
-    Screen('BlendFunction', win, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    %Screen('BlendFunction', win, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     Screen('TextSize', win, 18);
     if isfield(MonitorSpecs, 'gammaTable')
         Screen('LoadNormalizedGammaTable', win, MonitorSpecs.gammaTable); % correct gamma
@@ -84,15 +85,15 @@ try
     
     % prepare runs
     numBlocks                 = 2;  % must be even-numbered due to the blocking movement/imagery
-    RepsPerBlock              = 4; % number of repetitions per block (mouth, shoulder, forward)
+    RepsPerBlock              = 3; % number of repetitions per block (mouth, shoulder, forward)
     noRepetitionAtStartAndEnd = 3;
     noMoreThanXRepetitions    = 2;
     Runs = get_graspBlocks(noRepetitionAtStartAndEnd, noMoreThanXRepetitions, starting_condition,...
                            ISI, frame_s, numBlocks, RepsPerBlock);
     
     % compute fliptimes (subtract half frame)
-    PTBCueTime  = CueTime - (frame_s * 0.5); % 
-    PTBTaskTime = TaskTime - (frame_s * 0.5);  
+    PTBCueTime  = CueTime - (frame_s * 0.7); % 
+    PTBTaskTime = TaskTime - (frame_s * 0.7);  
 
     % Construct and start response queue
     KbQueueCreate([],TastenVector);
@@ -105,7 +106,7 @@ try
     DrawFormattedText(win, msg, 'center', 'center', [255 255 255]);
     VpixxMarkerZero(win);
     Screen('Flip', win);
-    KbQueueWait();
+    KbQueueWait([],[], GetSecs + W);
 
      % show blank for smooth transition to next page
      VpixxMarkerZero(win);
@@ -138,9 +139,9 @@ try
       VpixxMarkerZero(win);
       KbQueueFlush;    
       Screen('Flip', win);
-      KbQueueWait();
+      KbQueueWait([],[], GetSecs + W);
       
-      %% loop over trials                   
+      %% loop over trials    
         for ntrial = 1:size(trialMat, 1)
         
         % show black fixcross (ISI)
@@ -148,16 +149,26 @@ try
         VpixxMarkerZero(win);
         [TrialStart] = Screen('Flip', win);
        
+        switch trialMat(ntrial, 2)
+         case 5 % motion
         % show colored fixcross (cue)
         Screen('DrawLines', win, FixCross, FixCrossWidth, msf_colors{trialMat(ntrial,1)}, [xCenter, yCenter]);
         VpixxMarkerZero(win);
         [CueStart] = Screen('Flip', win, TrialStart + trialMat(ntrial, 3));
         
-        % show white fixcross (go signal)
-        Screen('DrawLines', win, FixCross, FixCrossWidth, colorWhite, [xCenter, yCenter]);
+        % show white circle (go signal)
+        Screen('GluDisk', win, colorWhite, xCenter, yCenter, FixCrossPixel/2);
         setVpixxMarker(win, trialMat(ntrial, 2));
         [TargetStart] = Screen('Flip', win, CueStart + PTBCueTime);
-        
+
+          case 6 % imagery
+        % show colored circle (go signal)
+        Screen('GluDisk', win, msf_colors{trialMat(ntrial,1)}, xCenter, yCenter, FixCrossPixel/2);
+        setVpixxMarker(win, trialMat(ntrial, 2));
+        [TargetStart] = Screen('Flip', win, TrialStart + PTBCueTime);
+        CueStart      = TrialStart;      
+        end
+
         % show question mark (imagery runs) or blank screen (movement
         % runs); get response key and response time
         Screen('TextSize', win, textSizeQuestionMark);
@@ -170,15 +181,16 @@ try
         end
         
         % change key codes [y x c v] to numerical values from 1 to 4
-        if (isnan(keyCode) | ~ismember([89, 88, 67, 86], keyCode))
+        if (isnan(keyCode) | ~any(ismember(TastenCodes(1:4), keyCode)))
         rating = NaN;
         else
-        rating = find(ismember([89, 88, 67, 86], keyCode));
+        rating = find(ismember(TastenCodes(1:4), keyCode));
         end
         
         % write condition codes and results into matrix
         protocolMatrix(ntrial, :) = [nblock, ntrial, keyCode, rating, responseTime, trialMat(ntrial, 1:3), ...
                 CueStart - TrialStart, TargetStart - CueStart, QuestionStart - TargetStart]; 
+        checkESC;
         end % end trial loop
         
         % write protocol matrix into cell (per run)
@@ -205,15 +217,15 @@ try
 % GraspMI.monitor_refresh    = hz;
 % GraspMI.MonitorDimension   = MonitorDimension;
 % 
-% save([rawdir, outfilename], 'GraspMI');
-
+% msave([rawdir, outfilename], 'GraspMI');
+% 
 % show ending message
 KbQueueFlush; 
 Screen('TextSize', win, textSize);
 DrawFormattedText(win, msgEnd, 'center', 'center', colorWhite);
 VpixxMarkerZero(win);
 Screen('Flip', win);
-KbQueueWait();
+KbQueueWait([],[], GetSecs + W);
 
 catch
     Screen('CloseAll');
